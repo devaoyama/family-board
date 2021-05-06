1ce550e9cb7ade13b7b672da6bda15d54754b568
devkeita <keita.mywork@gmail.com>
2021-05-06 23:59:55 +0900

    drawerコンポーネントを作成してHeaderに追加

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,34 +1,15 @@
-import React, {useState} from "react";
-import { IconButton } from "@material-ui/core";
-import AppBar from "@material-ui/core/AppBar";
-import Toolbar from "@material-ui/core/Toolbar";
-import MenuIcon from "@material-ui/icons/Menu";
-import Typography from "@material-ui/core/Typography";
-import Box from "@material-ui/core/Box";
-import GroupIcon from "@material-ui/icons/Group";
-import Drawer from "@material-ui/core/Drawer";
+import React from "react";
+import { MyAppBar } from "src/components/header/MyAppBar";
+import { HeaderDrawer } from "src/components/header/HeaderDrawer";
+import { useDrawer } from "src/hooks/header/useDrawer";
 
 export const Header: React.FC = () => {
-  const [open, setOpen] = useState(false);
+  const drawer = useDrawer();
 
   return (
     <React.Fragment>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton edge="start" color="inherit" aria-label="menu" onClick={() => setOpen(true)}>
-            <MenuIcon />
-          </IconButton>
-          <Box mx={"auto"}>
-            <Typography variant="h6">ファミリーボード</Typography>
-          </Box>
-          <IconButton edge="end" color="inherit" aria-label="menu">
-            <GroupIcon />
-          </IconButton>
-        </Toolbar>
-      </AppBar>
-      <Drawer anchor={"left"} open={open} onClose={() => setOpen(false)}>
-        リスト
-      </Drawer>
+      <MyAppBar onOpenDrawer={drawer.open} />
+      <HeaderDrawer isOpen={drawer.isOpen} onClose={drawer.close} />
     </React.Fragment>
   );
 };
