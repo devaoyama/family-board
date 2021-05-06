4044e362035717e14a9788a2667c1592b746a105
devkeita <keita.mywork@gmail.com>
2021-05-06 23:58:35 +0900

    Headerコンポーネントのディレクトリ変更

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import { IconButton } from "@material-ui/core";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
@@ -6,13 +6,16 @@ import MenuIcon from "@material-ui/icons/Menu";
 import Typography from "@material-ui/core/Typography";
 import Box from "@material-ui/core/Box";
 import GroupIcon from "@material-ui/icons/Group";
+import Drawer from "@material-ui/core/Drawer";
 
 export const Header: React.FC = () => {
+  const [open, setOpen] = useState(false);
+
   return (
     <React.Fragment>
       <AppBar position="static">
         <Toolbar>
-          <IconButton edge="start" color="inherit" aria-label="menu">
+          <IconButton edge="start" color="inherit" aria-label="menu" onClick={() => setOpen(true)}>
             <MenuIcon />
           </IconButton>
           <Box mx={"auto"}>
@@ -23,6 +26,9 @@ export const Header: React.FC = () => {
           </IconButton>
         </Toolbar>
       </AppBar>
+      <Drawer anchor={"left"} open={open} onClose={() => setOpen(false)}>
+        リスト
+      </Drawer>
     </React.Fragment>
   );
 };
