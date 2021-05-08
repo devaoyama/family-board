0b2cd0c62dfc8d663aa5d6677f2849c39db5154c
devkeita <keita.mywork@gmail.com>
2021-05-08 20:51:45 +0900

    family作成用のコンポーネントを作成

---
+++ src/components/families/create/CreateContainer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Container from "@material-ui/core/Container";
+import { FormContainer } from "src/components/families/create/FormContainer";
+
+export const CreateContainer: React.FC = () => {
+  return (
+    <Container>
+      <FormContainer />
+    </Container>
+  );
+};
