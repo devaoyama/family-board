0973425c6ab6d3ff460b210d83c4983591222cf6
devkeita <keita.mywork@gmail.com>
2021-05-08 18:17:17 +0900

    CssBaseLineを追加

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { AppProps } from "next/app";
 import { Auth0Provider } from "@auth0/auth0-react";
+import CssBaseline from "@material-ui/core/CssBaseline";
 import "../styles/globals.css";
 import { Apollo } from "src/components/common/Apollo";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <div suppressHydrationWarning={true}>
+      <CssBaseline />
       {typeof window === "undefined" ? null : (
         <Auth0Provider
           domain={process.env.REACT_APP_AUTH0_DOMAIN!}
