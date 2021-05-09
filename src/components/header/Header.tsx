56441f7be40b1118faf6a2ecf0a61dcb9286fba0
devkeita <keita.mywork@gmail.com>
2021-05-09 23:17:04 +0900

    Headerでgraphqlを叩いてデータを取得(仮)

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,15 +1,33 @@
 import React from "react";
+import { gql } from "@apollo/client/core";
+import { useQuery } from "@apollo/client";
 import { MyAppBar } from "src/components/header/MyAppBar";
 import { HeaderDrawer } from "src/components/header/HeaderDrawer";
 import { useDrawer } from "src/hooks/header/useDrawer";
+import { LoadingSpinner } from "src/components/common/LoadingSpinner";
+
+const HEADER_QUERY = gql`
+  query {
+    families {
+      id
+      name
+    }
+  }
+`;
 
 export const Header: React.FC = () => {
+  const { data, loading } = useQuery(HEADER_QUERY);
   const drawer = useDrawer();
 
   return (
     <React.Fragment>
       <MyAppBar onOpenDrawer={drawer.open} />
-      <HeaderDrawer isOpen={drawer.isOpen} onClose={drawer.close} />
+      <HeaderDrawer
+        families={loading ? [] : data.families}
+        isOpen={drawer.isOpen}
+        onClose={drawer.close}
+      />
+      {loading && <LoadingSpinner />}
     </React.Fragment>
   );
 };
