66db7e1ba455cb295324d6cabb0df43c6b2c4387
devkeita <keita.mywork@gmail.com>
2021-05-10 20:38:16 +0900

    familyを作成した後のcacheの設定

--- src/hooks/families/useCreateFamily.ts
+++ src/hooks/families/useCreateFamily.ts
@@ -10,6 +10,7 @@ const CREATE_FAMILY_MUTATION = gql`
   mutation CreateFamilyMutation($input: families_insert_input!) {
     insert_families_one(object: $input) {
       id
+      name
     }
   }
 `;
@@ -27,7 +28,29 @@ export const useCreateFamily = ({
   onCreateFamily,
   onCreateFamilyError,
 }: Args): TUseCreateFamily => {
-  const [createFamilyMutation] = useMutation<CreateFamilyMutation, CreateFamilyMutationVariables>(CREATE_FAMILY_MUTATION);
+  const [createFamilyMutation] = useMutation<CreateFamilyMutation, CreateFamilyMutationVariables>(
+    CREATE_FAMILY_MUTATION,
+    {
+      update(cache, { data }) {
+        cache.modify({
+          fields: {
+            families(existingFamilies = []) {
+              const newFamilyRef = cache.writeFragment({
+                data: data?.insert_families_one,
+                fragment: gql`
+                  fragment NewFamily on families {
+                    id
+                    name
+                  }
+                `,
+              });
+              return [...existingFamilies, newFamilyRef];
+            },
+          },
+        });
+      },
+    },
+  );
 
   const createFamily = useCallback(
     async (variables: CreateFamilyMutationVariables) => {
