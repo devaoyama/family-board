7faacc4abc61cc4f64834584cda84c9ce0c30642
devkeita <keita.mywork@gmail.com>
2021-05-13 16:12:34 +0900

    メンバーを追加したときにキャッシュが更新されるようにした

--- src/hooks/members/useCreateMember.tsx
+++ src/hooks/members/useCreateMember.tsx
@@ -5,21 +5,37 @@ import {
   CreateMemberMutation,
   CreateMemberMutationVariables,
 } from "src/hooks/members/__generated__/CreateMemberMutation";
+import { MEMBERS_FRAGMENT } from "src/components/header/MemberListItem";
 
 const CREATE_MEMBER_MUTATION = gql`
   mutation CreateMemberMutation($input: members_insert_input!) {
     insert_members_one(object: $input) {
       id
       name
+      user_id
     }
   }
 `;
 
-const NEW_MEMBER_FRAGMENT = gql`
-  fragment NewMemberFragment on members {
-    id
-    name
+const GET_CURRENT_USER_QUERY = gql`
+  query NewGetCurrentUserQuery {
+    get_current_user {
+      id
+      name
+      current_family {
+        id
+        name
+        family_members {
+          member {
+            id
+            user_id
+            ...MemberFragment
+          }
+        }
+      }
+    }
   }
+  ${MEMBERS_FRAGMENT}
 `;
 
 type Args = {
@@ -44,16 +60,23 @@ export const useCreateMember = ({
     CREATE_MEMBER_MUTATION,
     {
       update(cache, { data }) {
-        cache.modify({
-          fields: {
-            members(existingMembers = []) {
-              const newMemberRef = cache.writeFragment({
-                data: data?.insert_members_one,
-                fragment: NEW_MEMBER_FRAGMENT,
-              });
-              return [...existingMembers, newMemberRef];
-            },
+        const currentUser: any = cache.readQuery({ query: GET_CURRENT_USER_QUERY });
+        const newGetCurrentUser = {
+          ...currentUser.get_current_user[0],
+          current_family: {
+            ...currentUser.get_current_user[0].current_family,
+            family_members: [
+              ...currentUser.get_current_user[0].current_family.family_members,
+              {
+                member: data?.insert_members_one,
+                __typename: "family_member",
+              },
+            ],
           },
+        };
+        cache.writeQuery({
+          query: GET_CURRENT_USER_QUERY,
+          data: { get_current_user: [newGetCurrentUser] },
         });
       },
     },
