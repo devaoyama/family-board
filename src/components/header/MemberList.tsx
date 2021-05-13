e99a0f4503e04f2f7c06e1833f7498df9a4d1737
devkeita <keita.mywork@gmail.com>
2021-05-13 12:12:34 +0900

    MemberListを修正

--- src/components/header/MemberList.tsx
+++ src/components/header/MemberList.tsx
@@ -1,34 +1,27 @@
 import List from "@material-ui/core/List";
 import ListSubheader from "@material-ui/core/ListSubheader";
 import React from "react";
-import { gql } from "@apollo/client/core";
 import { MemberListItem } from "src/components/header/MemberListItem";
 import { AddMemberListItem } from "src/components/header/AddMemberListItem";
-import { MemberFragment } from "src/components/header/__generated__/MemberFragment";
-
-export const MEMBERS_FRAGMENT = gql`
-  fragment MemberFragment on members {
-    id
-    name
-  }
-`;
+import { HeaderQuery_get_current_user_current_family_family_members } from "src/components/header/__generated__/HeaderQuery";
 
 type Props = {
-  me: MemberFragment;
-  members: MemberFragment[];
+  me?: HeaderQuery_get_current_user_current_family_family_members;
+  familyMembers?: HeaderQuery_get_current_user_current_family_family_members[];
   onClickAddMemberListItem: () => void;
 };
 
-export const MemberList: React.FC<Props> = ({ me, members, onClickAddMemberListItem }) => {
+export const MemberList: React.FC<Props> = ({ me, familyMembers, onClickAddMemberListItem }) => {
   return (
     <>
       <List subheader={<ListSubheader component="div">自分</ListSubheader>}>
-        <MemberListItem name={me.name} />
+        {me && <MemberListItem member={me.member} />}
       </List>
       <List subheader={<ListSubheader component="div">メンバー</ListSubheader>}>
-        {members.map((member) => (
-          <MemberListItem key={member.id} name={member.name} />
-        ))}
+        {familyMembers &&
+          familyMembers.map((familyMember) => (
+            <MemberListItem key={familyMember.member.id} member={familyMember.member} />
+          ))}
         <AddMemberListItem onClickAddMemberListItem={onClickAddMemberListItem} />
       </List>
     </>
--- src/components/header/MemberListItem.tsx
+++ src/components/header/MemberListItem.tsx
@@ -3,18 +3,27 @@ import ListItemIcon from "@material-ui/core/ListItemIcon";
 import ListItemText from "@material-ui/core/ListItemText";
 import ListItem from "@material-ui/core/ListItem";
 import AccountCircleIcon from "@material-ui/icons/AccountCircle";
+import { gql } from "@apollo/client/core";
+import { MemberFragment } from "src/components/header/__generated__/MemberFragment";
+
+export const MEMBERS_FRAGMENT = gql`
+  fragment MemberFragment on members {
+    id
+    name
+  }
+`;
 
 type Props = {
-  name: string;
+  member: MemberFragment;
 };
 
-export const MemberListItem: React.FC<Props> = ({ name }) => {
+export const MemberListItem: React.FC<Props> = ({ member }) => {
   return (
     <ListItem button>
       <ListItemIcon>
         <AccountCircleIcon fontSize={"large"} />
       </ListItemIcon>
-      <ListItemText primary={name} />
+      <ListItemText primary={member.name} />
     </ListItem>
   );
 };
