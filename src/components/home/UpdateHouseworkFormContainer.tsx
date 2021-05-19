a53eb1589fb319eb0afaae3ceda2e3225f2be6d6
devkeita <keita.mywork@gmail.com>
2021-05-19 22:35:31 +0900

    コードの最適化

--- src/components/home/UpdateHouseworkFormContainer.tsx
+++ src/components/home/UpdateHouseworkFormContainer.tsx
@@ -67,87 +67,85 @@ export const UpdateHouseworkFormContainer: React.FC<Props> = ({
   );
 
   return (
-    <React.Fragment>
-      <Dialog open={isOpen} onClose={onClose} fullWidth>
-        <DialogTitle disableTypography>
-          <Typography variant={"h6"}>{housework.title}</Typography>
-          <IconButton
-            aria-label="close"
-            onClick={onClickDeleteButton}
-            className={classes.deleteButton}
-          >
-            <DeleteIcon />
-          </IconButton>
-        </DialogTitle>
-        <DialogContent>
-          <Controller
-            name="title"
-            control={control}
-            rules={{ required: "タイトルは必須です。" }}
-            render={({ field: { onChange, value } }) => (
-              <TextField
-                type="text"
-                label="タイトル"
-                value={value}
-                onChange={onChange}
-                error={Boolean(errors.title)}
-                helperText={errors.title?.message}
-                required
-                fullWidth
-                margin="normal"
-              />
-            )}
-          />
-          <Controller
-            name="description"
-            control={control}
-            render={({ field: { onChange, value } }) => (
-              <TextField
-                type="text"
-                label="説明"
-                value={value}
-                onChange={onChange}
-                error={Boolean(errors.description)}
-                helperText={errors.description?.message}
-                margin="normal"
-                rows={4}
-                fullWidth
-                multiline
-              />
-            )}
-          />
-          <Controller
-            name="point"
-            control={control}
-            render={({ field: { onChange, value } }) => (
-              <TextField
-                type="number"
-                label="ポイント"
-                value={value}
-                onChange={onChange}
-                error={Boolean(errors.point)}
-                helperText={errors.point?.message}
-                required
-                fullWidth
-                margin="normal"
-              />
-            )}
-          />
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={onClose} color="default">
-            キャンセル
-          </Button>
-          <Box mx="auto" />
-          <Button
-            onClick={handleSubmit(onClickUpdateHousework)}
-            disabled={isSubmitting}
-            color="primary"
-          >
-            更新
-          </Button>
-        </DialogActions>
-      </Dialog>
-    </React.Fragment>
+    <Dialog open={isOpen} onClose={onClose} fullWidth>
+      <DialogTitle disableTypography>
+        <Typography variant={"h6"}>{housework.title}</Typography>
+        <IconButton
+          aria-label="close"
+          onClick={onClickDeleteButton}
+          className={classes.deleteButton}
+        >
+          <DeleteIcon />
+        </IconButton>
+      </DialogTitle>
+      <DialogContent>
+        <Controller
+          name="title"
+          control={control}
+          rules={{ required: "タイトルは必須です。" }}
+          render={({ field: { onChange, value } }) => (
+            <TextField
+              type="text"
+              label="タイトル"
+              value={value}
+              onChange={onChange}
+              error={Boolean(errors.title)}
+              helperText={errors.title?.message}
+              required
+              fullWidth
+              margin="normal"
+            />
+          )}
+        />
+        <Controller
+          name="description"
+          control={control}
+          render={({ field: { onChange, value } }) => (
+            <TextField
+              type="text"
+              label="説明"
+              value={value}
+              onChange={onChange}
+              error={Boolean(errors.description)}
+              helperText={errors.description?.message}
+              margin="normal"
+              rows={4}
+              fullWidth
+              multiline
+            />
+          )}
+        />
+        <Controller
+          name="point"
+          control={control}
+          render={({ field: { onChange, value } }) => (
+            <TextField
+              type="number"
+              label="ポイント"
+              value={value}
+              onChange={onChange}
+              error={Boolean(errors.point)}
+              helperText={errors.point?.message}
+              required
+              fullWidth
+              margin="normal"
+            />
+          )}
+        />
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={onClose} color="default">
+          キャンセル
+        </Button>
+        <Box mx="auto" />
+        <Button
+          onClick={handleSubmit(onClickUpdateHousework)}
+          disabled={isSubmitting}
+          color="primary"
+        >
+          更新
+        </Button>
+      </DialogActions>
+    </Dialog>
   );
 };
