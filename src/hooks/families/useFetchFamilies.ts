16728600a01ee2bba5b6b269c195b8d28763eeca
devkeita <keita.mywork@gmail.com>
2021-05-22 23:14:15 +0900

    家族を取得するhooksを作成

---
+++ src/hooks/families/useFetchFamilies.ts
@@ -0,0 +1,34 @@
+import { useQuery } from "@apollo/client";
+import { gql } from "@apollo/client/core";
+import {
+  FetchFamiliesQuery,
+  FetchFamiliesQuery_families,
+} from "src/hooks/families/__generated__/FetchFamiliesQuery";
+
+const FETCH_FAMILIES_QUERY = gql`
+  query FetchFamiliesQuery {
+    families {
+      id
+      name
+      family_members {
+        member {
+          id
+          user_id
+          name
+        }
+      }
+    }
+  }
+`;
+
+type Props = {
+  families: FetchFamiliesQuery_families[];
+};
+
+export const useFetchFamilies = (): Props => {
+  const { data } = useQuery<FetchFamiliesQuery>(FETCH_FAMILIES_QUERY);
+
+  return {
+    families: data?.families || [],
+  };
+};
