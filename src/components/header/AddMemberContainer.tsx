b75a07f55e58e738bd1d0428c06bb3cdf8c7fceb
devkeita <keita.mywork@gmail.com>
2021-05-24 21:24:48 +0900

    ロジックはContainerで呼び出すようにした

--- src/components/header/AddMemberContainer.tsx
+++ src/components/header/AddMemberContainer.tsx
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useCallback } from "react";
 import Container from "@material-ui/core/Container";
 import IconButton from "@material-ui/core/IconButton";
 import BackspaceIcon from "@material-ui/icons/Backspace";
 import { CreateMemberFormContainer } from "src/components/header/CreateMemberFormContainer";
 import { FetchFamiliesQuery_families } from "src/hooks/families/__generated__/FetchFamiliesQuery";
+import { useCreateMember } from "src/hooks/members/useCreateMember";
 
 type Props = {
   onClickBackButton: () => void;
@@ -11,16 +12,32 @@ type Props = {
 };
 
 export const AddMemberContainer: React.FC<Props> = ({ onClickBackButton, currentFamily }) => {
+  const { createMember } = useCreateMember({
+    currentFamilyId: currentFamily?.id || 0,
+    onCreateMember: () => {
+      onClickBackButton();
+    },
+    onCreateMemberError: () => {
+      // todo エラーToastを表示
+    },
+  });
+
+  const onClickCreateMember = useCallback(
+    async (data) => {
+      if (currentFamily?.id) {
+        await createMember({ name: data.nickname });
+      }
+    },
+    [currentFamily],
+  );
+
   return (
     <>
       <IconButton aria-label="back" onClick={onClickBackButton}>
         <BackspaceIcon fontSize={"large"} />
       </IconButton>
       <Container>
-        <CreateMemberFormContainer
-          currentFamily={currentFamily}
-          onSuccessAddMember={onClickBackButton}
-        />
+        <CreateMemberFormContainer onClickCreateMember={onClickCreateMember} />
       </Container>
     </>
   );
