f27d489e172ee20fd65fb5ede8159930176de831
devkeita <keita.mywork@gmail.com>
2021-05-25 23:16:38 +0900

    ToastServiceProviderを作成

--- src/components/common/ToastServiceProvider.tsx
+++ src/components/common/ToastServiceProvider.tsx
@@ -1,11 +1,39 @@
-import { createContext } from "react";
+import React, { createContext, useState } from "react";
+import Snackbar, { SnackbarProps } from "@material-ui/core/Snackbar";
 
 type ToastServiceContextProps = {
-  isVisible: boolean;
+  show: (snackbarProps: SnackbarProps) => void;
 };
 
-const defaultProps: ToastServiceContextProps = {
-  isVisible: true,
+const ToastServiceContext = createContext<ToastServiceContextProps>({
+  show: () => null,
+});
+
+type ToastServiceProviderProps = {
+  defaultSnackbarProps: SnackbarProps;
 };
 
-const ToastServiceContext = createContext<ToastServiceContextProps>(defaultProps);
+export const ToastServiceProvider: React.FC<ToastServiceProviderProps> = ({
+  children,
+  defaultSnackbarProps,
+}) => {
+  const [snackbarProps, setSnackbarProps] = useState<SnackbarProps>({
+    ...defaultSnackbarProps,
+    onClose: () => setSnackbarProps((v) => ({ ...v, open: false })),
+  });
+
+  const show = (props: Omit<SnackbarProps, "open">) => {
+    setSnackbarProps({
+      ...props,
+      open: true,
+      onClose: snackbarProps.onClose,
+    });
+  };
+
+  return (
+    <ToastServiceContext.Provider value={{ show }}>
+      {children}
+      <Snackbar {...snackbarProps} />
+    </ToastServiceContext.Provider>
+  );
+};
